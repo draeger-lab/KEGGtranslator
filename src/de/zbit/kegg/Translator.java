4c1f5b5a6fac2a95fe5f29aa0f294d9859101cf4
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2010-10-28 12:43:44 +0000

    Changed main to TreeNodes to ensure the order of the configuration.

--- src/de/zbit/kegg/Translator.java
+++ src/de/zbit/kegg/Translator.java
@@ -1,8 +1,8 @@
 package de.zbit.kegg;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.InvalidPropertiesFormatException;
+import java.util.TreeMap;
 import java.util.prefs.BackingStoreException;
 
 import org.sbml.tolatex.LaTeXOptions;
@@ -19,25 +19,26 @@ import de.zbit.util.SBPreferences;
  */
 public class Translator {
 
-	/**
-	 * @param args
-	 * @throws BackingStoreException
-	 * @throws IOException
-	 * @throws InvalidPropertiesFormatException
-	 */
-	public static void main(String[] args)
-			throws InvalidPropertiesFormatException, IOException,
-			BackingStoreException {
-		String usage = "java Translator [options]";
-		HashMap<String, Class<?>> defFileAndKeys = new HashMap<String, Class<?>>();
-		defFileAndKeys.put(TranslatorOptions.CONFIG_FILE_LOCATION,
-				TranslatorOptions.class);
-		defFileAndKeys.put(GUIOptions.CONFIG_FILE_LOCATION, GUIOptions.class);
-		defFileAndKeys.put(LaTeXOptions.CONFIG_FILE_LOCATION,
-				LaTeXOptions.class);
-		SBPreferences.analyzeCommandLineArguments(defFileAndKeys, usage, args);
-		SettingsDialog d = new SettingsDialog("Settings");
-		d.showSettingsDialog();
-	}
+    /**
+     * @param args
+     * @throws BackingStoreException
+     * @throws IOException
+     * @throws InvalidPropertiesFormatException
+     */
+    public static void main(String[] args)
+	    throws InvalidPropertiesFormatException, IOException,
+	    BackingStoreException {
+	String usage = "java Translator [options]";
+	TreeMap<String, Class<?>> defFileAndKeys = new TreeMap<String, Class<?>>();
+	defFileAndKeys.put(TranslatorOptions.CONFIG_FILE_LOCATION,
+		TranslatorOptions.class);
+	defFileAndKeys.put(GUIOptions.CONFIG_FILE_LOCATION, GUIOptions.class);
+	defFileAndKeys.put(LaTeXOptions.CONFIG_FILE_LOCATION,
+		LaTeXOptions.class);
+
+	SBPreferences.analyzeCommandLineArguments(defFileAndKeys, usage, args);
+	SettingsDialog d = new SettingsDialog("Settings");
+	d.showSettingsDialog();
+    }
 
 }
