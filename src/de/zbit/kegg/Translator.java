f70ddd2c61648e5de5ae402fd59ac1fa0a1f1190
Clemens Wrzodek <clemens@germancafe.de>
2010-11-02 13:10:30 +0000

    Removed XML-file dependency for Options.

--- src/de/zbit/kegg/Translator.java
+++ src/de/zbit/kegg/Translator.java
@@ -1,16 +1,11 @@
 package de.zbit.kegg;
 
-import java.awt.Dialog;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.TreeMap;
 import java.util.prefs.BackingStoreException;
 
-import de.zbit.gui.GUIOptions;
-import de.zbit.gui.cfg.PreferencesDialog;
 import de.zbit.kegg.gui.TranslatorUI;
 import de.zbit.util.SBPreferences;
-import de.zbit.util.SBProperties;
 
 /**
  * This class is the main class for the KEGGTranslator project.
@@ -19,32 +14,33 @@ import de.zbit.util.SBProperties;
  * @date 2010-10-25
  */
 public class Translator {
-
-	/**
-	 * @param args
-	 * @throws BackingStoreException
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	public static void main(String[] args) throws IOException,
-			BackingStoreException, URISyntaxException {
-		TreeMap<String, Class<?>> defFileAndKeys = new TreeMap<String, Class<?>>();
-
-		defFileAndKeys.put(TranslatorOptions.CONFIG_FILE_LOCATION,
-				TranslatorOptions.class);
-
-		defFileAndKeys.put(GUIOptions.CONFIG_FILE_LOCATION, GUIOptions.class);
-		// defFileAndKeys.put(LaTeXOptions.CONFIG_FILE_LOCATION,LaTeXOptions.class);
-
-		SBProperties props = SBPreferences.analyzeCommandLineArguments(
-				defFileAndKeys, args);
-
+  /**
+   * @param args
+   * @throws BackingStoreException
+   * @throws IOException
+   * @throws URISyntaxException 
+   */
+	public static void main(String[] args) throws IOException, BackingStoreException, URISyntaxException {
+		//TreeMap<String, Class<?>> defFileAndKeys = new TreeMap<String, Class<?>>();
+		//defFileAndKeys.put(TranslatorOptions.CONFIG_FILE_LOCATION,TranslatorOptions.class);
+		//defFileAndKeys.put(GUIOptions.CONFIG_FILE_LOCATION, GUIOptions.class);
+		//defFileAndKeys.put(LaTeXOptions.CONFIG_FILE_LOCATION,LaTeXOptions.class);
+		//SBProperties props = SBPreferences.analyzeCommandLineArguments(defFileAndKeys, args);
+		
+		SBPreferences.analyzeCommandLineArguments(TranslatorOptions.class, args);
+		
+		// Demo
+		//PreferencesDialog d = new PreferencesDialog((Dialog)null);
+		//PreferencesDialog.showPreferencesDialog();
+		
 		// Should we start the GUI?
-		if (Boolean.parseBoolean(props.getProperty(GUIOptions.GUI.toString())
-				.toString())) {
+		//  Boolean.parseBoolean(props.getProperty(GUIOptions.GUI.toString()).toString())
+		if (args.length<1) {
 			new TranslatorUI();
+		} else {
+			// TODO: Implement no-gui functionality.
 		}
-
+		
 	}
-
+  
 }
--- src/de/zbit/kegg/TranslatorOptions.java
+++ src/de/zbit/kegg/TranslatorOptions.java
@@ -24,47 +24,47 @@ public abstract interface TranslatorOptions {
    * Most important options: input, output and file format.
    */
   
-  public static final Option INPUT = new Option("INPUT",File.class,
-      "Path and name of the source, KGML formatted, XML-file.", (short) 2, "-i");  
+  public static final Option<File> INPUT = new Option<File>("INPUT",File.class,
+      "Path and name of the source, KGML formatted, XML-file.", (short) 2, "-i", new File(System.getProperty("user.dir")));
 
-  public static final Option OUTPUT = new Option("OUTPUT",File.class,
-      "Path and name, where the translated file should be put.", (short) 2, "-o");
+  public static final Option<File> OUTPUT = new Option<File>("OUTPUT",File.class,
+      "Path and name, where the translated file should be put.", (short) 2, "-o", new File(System.getProperty("user.dir")));
 
-  public static final Option FORMAT = new Option("FORMAT",String.class,
-      "Target file format for the translation.","{SBML,LaTeX,GraphML,GML,JPG,GIF,TGF,YGF}" ,(short) 2, "-f");
+  public static final Option<String> FORMAT = new Option<String>("FORMAT",String.class,
+      "Target file format for the translation.","{SBML,LaTeX,GraphML,GML,JPG,GIF,TGF,YGF}" ,(short) 2, "-f", "SBML");
   
   /*
    * Generic translation options
    */
   
-  public static final Option REMOVE_ORPHANS = new Option("REMOVE_ORPHANS",Boolean.class,
-      "If true, remove all nodes that have no edges, before translating the pathway.", (short) 2, "-ro");
+  public static final Option<Boolean> REMOVE_ORPHANS = new Option<Boolean>("REMOVE_ORPHANS",Boolean.class,
+      "If true, remove all nodes that have no edges, before translating the pathway.", (short) 2, "-ro", false);
 
-  public static final Option SHORT_NAMES = new Option("SHORT_NAMES",Boolean.class,
-      "If true, shows only short names of all KEGG entries.");
+  public static final Option<Boolean> SHORT_NAMES = new Option<Boolean>("SHORT_NAMES",Boolean.class,
+      "If true, shows only short names of all KEGG entries.", true);
   
-  public static final Option REMOVE_WHITE_GENE_NODES = new Option("REMOVE_WHITE_GENE_NODES",Boolean.class,
-      "If true, removes all gene-nodes in the KEGG document, which are white.");
+  public static final Option<Boolean> REMOVE_WHITE_GENE_NODES = new Option<Boolean>("REMOVE_WHITE_GENE_NODES",Boolean.class,
+      "If true, removes all gene-nodes in the KEGG document, which are white.", true);
 
-  public static final Option AUTOCOMPLETE_REACTIONS = new Option("AUTOCOMPLETE_REACTIONS",Boolean.class,
-      "If true, automatically looks for missing reactants and enzymes of reactions and adds them to the document.", (short) 2, "-ar");
+  public static final Option<Boolean> AUTOCOMPLETE_REACTIONS = new Option<Boolean>("AUTOCOMPLETE_REACTIONS",Boolean.class,
+      "If true, automatically looks for missing reactants and enzymes of reactions and adds them to the document.", (short) 2, "-ar", true);
   
   // =! retrieveKeggAnnots
-  public static final Option OFFLINE_MODE = new Option("OFFLINE_MODE",Boolean.class,
-      "If true, no additional information will be retrieved from the KEGG-Server.");
+  public static final Option<Boolean> OFFLINE_MODE = new Option<Boolean>("OFFLINE_MODE",Boolean.class,
+      "If true, no additional information will be retrieved from the KEGG-Server.", false);
   
   /*
    * Graphical, yFiles based translations
    */
   
-  public static final Option MERGE_NODES_WITH_SAME_EDGES = new Option("MERGE_NODES_WITH_SAME_EDGES",Boolean.class,
-      "If true, merges all nodes that have exactly the same relations (sources, targets and types).", (short) 2, "--merge");
+  public static final Option<Boolean> MERGE_NODES_WITH_SAME_EDGES = new Option<Boolean>("MERGE_NODES_WITH_SAME_EDGES",Boolean.class,
+      "If true, merges all nodes that have exactly the same relations (sources, targets and types).", (short) 2, "--merge", false);
   
   /*
    * Funcional, SBML based translations
    */
   
-  public static final Option CELLDESIGNER_ANNOTATIONS = new Option("CELLDESIGNER_ANNOTATIONS",Boolean.class,
-      "If true, adds celldesigner annotations to the SBML-XML document.", (short) 2, "-cd");
+  public static final Option<Boolean> CELLDESIGNER_ANNOTATIONS = new Option<Boolean>("CELLDESIGNER_ANNOTATIONS",Boolean.class,
+      "If true, adds celldesigner annotations to the SBML-XML document.", (short) 2, "-cd", false);
   
 }
